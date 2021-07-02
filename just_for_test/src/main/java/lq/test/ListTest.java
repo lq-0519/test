9a65bdd480c9f14ae38da9d70d5f1b2d6d142f18
liqian477 <liqian477@jd.com>
2021-07-02 09:45:21 +0800

    添加新模块

---
+++ just_for_test/src/main/java/lq/test/ListTest.java
@@ -0,0 +1,18 @@
+package lq.test;
+
+import java.util.ArrayList;
+
+public class ListTest {
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add(null);
+        strings.add(null);
+        strings.add(null);
+        strings.add(null);
+        strings.add(null);
+        System.out.println("strings.size() = " + strings.size());
+        for (String string : strings) {
+            System.out.println(string.length());
+        }
+    }
+}
