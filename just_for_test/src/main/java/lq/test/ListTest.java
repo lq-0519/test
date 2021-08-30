43671fe6df64f34afa92be30deae27ceaf97305a
liqian477 <liqian477@jd.com>
2021-08-30 22:19:05 +0800

    -

--- just_for_test/src/main/java/lq/test/ListTest.java
+++ just_for_test/src/main/java/lq/test/ListTest.java
@@ -31,7 +31,6 @@ public class ListTest {
     private static final ThreadLocal<Man> THREAD_LOCAL = new ThreadLocal<>();
 
     public static void main(String[] args) {
-        m13();
     }
 
     private static void m13() {
