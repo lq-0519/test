4eae4111ad63536208cfcc16eefcbaf752b6e6c4
liqian477 <liqian477@jd.com>
2021-09-22 09:57:07 +0800

    测试map扩容

--- just_for_test/src/main/java/lq/test/ListTest.java
+++ just_for_test/src/main/java/lq/test/ListTest.java
@@ -7,6 +7,8 @@ import org.apache.rocketmq.common.ThreadFactoryImpl;
 import org.springframework.beans.BeanUtils;
 import org.springframework.util.Assert;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -39,7 +41,30 @@ public class ListTest {
     private static int bucketSize = 0;
     private static LinkedList<Integer> integers = new LinkedList<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        //指定初始容量15来创建一个HashMap
+        HashMap map = new HashMap(1);
+        //获取HashMap整个类
+        Class<?> mapType = map.getClass();
+        //获取指定属性，也可以调用getDeclaredFields()方法获取属性数组
+        Field threshold = mapType.getDeclaredField("threshold");
+        //将目标属性设置为可以访问
+        threshold.setAccessible(true);
+        //获取指定方法，因为HashMap没有容量这个属性，但是capacity方法会返回容量值
+        Method capacity = mapType.getDeclaredMethod("capacity");
+        //设置目标方法为可访问
+        capacity.setAccessible(true);
+        //打印刚初始化的HashMap的容量、阈值和元素数量
+        System.out.println("容量：" + capacity.invoke(map) + "    阈值：" + threshold.get(map) + "    元素数量：" + map.size());
+        for (int i = 0; i < 17; i++) {
+            map.put(i, i);
+            //动态监测HashMap的容量、阈值和元素数量
+            System.out.println("容量：" + capacity.invoke(map) + "    阈值：" + threshold.get(map) + "    元素数量：" + map.size());
+        }
+    }
+
+    private static void m920() {
         String s = "你好!";
         System.out.println("s.length() = " + s.length());
         String s1 = "adf!";
